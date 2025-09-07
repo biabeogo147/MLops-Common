3f274b7ce51eb5cde8d91ab3a16d207f3161bebc
biabeogo147 <biabeogo147@gmail.com>
2025-09-07 12:20:28 +0700

    update create secret

--- create-secret.sh
+++ create-secret.sh
@@ -3,4 +3,5 @@
 kubectl create secret generic anime-recommender-secrets \
   --from-literal=GOOGLE_API_KEY="" \
   --from-literal=HUGGINGFACEHUB_API_TOKEN="" \
+  -n anime-recommender \
   --dry-run=client -o yaml | kubectl apply -f -
